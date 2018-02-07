7dedc640b3800ecb223e8dc29e100dc7ca16f6d1
Nat <nat@cmmc.io>
2018-02-07 17:57:21 +0700

    - add dump method

--- examples/example1/example1.ino
+++ examples/example1/example1.ino
@@ -1,12 +1,26 @@
 #include <Arduino.h>
 #include <CMMC_Packet.h>
 
-CMMC_Packet packet;
+
+const uint8_t PROJECT = 1;
+const uint8_t VERSION = 1;
+
+uint8_t header[2] = {0xfd, 0xfa};
+uint8_t tail[2] = {0x0d, 0x0a};
+
+CMMC_Packet packet(PROJECT, VERSION, header, tail);
 
 void setup()
 {
 	Serial.begin(57600); 
 	Serial.println("program started.");
+	packet.debug([](char *s) {
+		Serial.print(s); 
+	});
+	packet.dump();
+
+	// packet
+	// CMMC_PACKET_T *ptr = packet.getPacketPtr();
 }
 
 void loop()
--- src/CMMC_Packet.h
+++ src/CMMC_Packet.h
@@ -3,7 +3,6 @@
 
 #include <Arduino.h>
 
-
 #define DEBUG_BUFFER_SIZE 500
 #define USER_DEBUG_PRINTF(fmt, args...) { \
     sprintf(this->debug_buffer, fmt, ## args); \
@@ -29,7 +28,7 @@ typedef struct __attribute((__packed__)) {
 } CMMC_SENSOR_T;
 
 typedef struct __attribute((__packed__)) {
-  uint8_t header[2] = {0x7e, 0x7f};
+  uint8_t header[2] = {0x00, 0x00};
   uint8_t version = 1;
   uint8_t project = 1;
   uint8_t reserved[4]= {0xff,0xff,0xff,0xff};
@@ -37,20 +36,20 @@ typedef struct __attribute((__packed__)) {
   uint32_t ms;
   CMMC_SENSOR_T data;
   uint32_t sum;
-  uint8_t tail[2] = {0x0d, 0x0a};
+  uint8_t tail[2] = {0x00, 0x00};
 } CMMC_PACKET_T;
 
 #ifndef CMMC_NO_ALIAS
   #define CMMC_Packet CMMC_Packet
 #endif
 
-#ifdef ESP8266
-  extern "C" {
-    #include "user_interface.h"
-  }
-  #include "ESP8266WiFi.h"
-  #include <functional>
-#endif
+// #ifdef ESP8266
+//   extern "C" {
+//     #include "user_interface.h"
+//   }
+//   #include "ESP8266WiFi.h"
+//   #include <functional>
+// #endif
 
 typedef void (*cmmc_debug_cb_t)(const char* message);
 
@@ -58,8 +57,12 @@ class CMMC_Packet
 {
     public:
       // constructor
-      CMMC_Packet() {
-        // auto blank = [](const char* message) {};
+      CMMC_Packet(uint8_t project, uint8_t version, uint8_t header[2], uint8_t footer[2]) { 
+        this->_packet.header[0] = header[0];
+        this->_packet.header[1] = header[1];
+        this->_packet.tail[0] = footer[0];
+        this->_packet.tail[1] = footer[1];
+        // auto blank = [](const char* message) {}; 
       } 
       ~CMMC_Packet() {} 
       
@@ -74,12 +77,17 @@ class CMMC_Packet
           }
           return sum;
         } 
-        void dump(const u8* data, size_t size) {
-            for (size_t i = 0; i < size; i++) {
-              USER_DEBUG_PRINTF("%02x ", data[i]);
-            }
-            Serial.println();
-        } 
+
+      void dump() {
+        this->dump((u8*)&this->_packet, sizeof(this->_packet));
+      }
+
+      void dump(const u8* data, size_t size) {
+          for (size_t i = 0; i < size; i++) {
+            USER_DEBUG_PRINTF("%02x ", data[i]);
+          }
+          USER_DEBUG_PRINTF("\n");
+      } 
       void debug(cmmc_debug_cb_t); 
     private:
       char debug_buffer[DEBUG_BUFFER_SIZE];
