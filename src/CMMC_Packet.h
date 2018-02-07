2d4ac26578694f2a65b8adcdc4394e256998bcda
Nat <nat@cmmc.io>
2018-02-07 20:40:09 +0700

    move function body to cpp

--- src/CMMC_Packet.h
+++ src/CMMC_Packet.h
@@ -67,89 +67,20 @@ class CMMC_Packet
 {
   public:
     // constructor
-    CMMC_Packet(uint8_t project, uint8_t version, uint8_t header[2], uint8_t footer[2]) {
-      this->_packet.header[0] = header[0];
-      this->_packet.header[1] = header[1];
-      this->_packet.tail[0] = footer[0];
-      this->_packet.tail[1] = footer[1];
-
-      this->_packet.ms = millis();
-
-      // auto blank = [](const char* message) {};
-    }
-    ~CMMC_Packet() {}
+    CMMC_Packet(uint8_t project, uint8_t version, uint8_t header[2], uint8_t footer[2]);
+    ~CMMC_Packet(); 
 
     void init();
-    const char* getHexString() { 
-      toHexString((u8*)&this->_packet, sizeof(this->_packet), this->_hexStringBuffer) ;
-      return this->_hexStringBuffer;
-    }
+    const char* getHexString(); 
+    void toHexString(const u8 array[], size_t len, char buffer[]); 
+    const CMMC_PACKET_T* getPacketPtr(); 
+    uint32_t checksum(uint8_t* data, size_t len); 
+    void dump(); 
+    void updatePacketSum(); 
+    void dump(const u8* data, size_t size);
 
-    void toHexString(const u8 array[], size_t len, char buffer[])
-    {
-      for (unsigned int i = 0; i < len; i++)
-      {
-        byte nib1 = (array[i] >> 4) & 0x0F;
-        byte nib2 = (array[i] >> 0) & 0x0F;
-        buffer[i * 2 + 0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
-        buffer[i * 2 + 1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
-      }
-      buffer[len * 2] = '\0';
-    }
-
-    const CMMC_PACKET_T* getPacketPtr() {
-      return &this->_packet;
-    }
-
-    uint32_t checksum(uint8_t* data, size_t len) {
-      uint32_t sum = 0;
-      while (len--) {
-        sum += *(data++);
-      }
-      return sum;
-    }
-
-    void dump() {
-      this->dump((u8*)&this->_packet, sizeof(this->_packet));
-    }
-
-    void updatePacketSum() {
-      unsigned long packetSize = sizeof(this->_packet) - sizeof(this->_packet.sum);
-      this->_packet.sum = CMMC_Packet::checksum((uint8_t*) &this->_packet, packetSize);
-    }
-
-    void dump(const u8* data, size_t size) {
-      for (size_t i = 0; i < size; i++) {
-        USER_DEBUG_PRINTF("%02x ", data[i]);
-      }
-      USER_DEBUG_PRINTF("\n");
-    }
-
-    bool setSensorName(const char name[16]) {
-      int len = strlen(name);
-      if (len > 15) {
-        return false;
-      }
-      else {
-        strcpy(this->_packet.data.sensorName, name);
-        this->_packet.data.nameLen = strlen(name);
-      }
-      updatePacketSum();
-      return true;
-    }
-
-    bool setName(const char name[16]) {
-      int len = strlen(name);
-      if (len > 15) {
-        return false;
-      }
-      else {
-        strcpy(this->_packet.myName, name);
-        this->_packet.nameLen = strlen(name);
-      }
-      updatePacketSum();
-      return true;
-    }
+    bool setSensorName(const char name[16]);
+    bool setName(const char name[16]);
     void debug(cmmc_debug_cb_t);
   private:
     char debug_buffer[DEBUG_BUFFER_SIZE];
